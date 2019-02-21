66deaca19e468edb63f5bb68da5da60d0c97bbc8
Alexey Vekshin <alex@maxidom.ru>
2019-02-21 19:04:27 +0300

    fix error reporting for "split" filter plugin

--- filter_plugins/split.py
+++ filter_plugins/split.py
@@ -1,17 +1,19 @@
 from ansible import errors
 import re
 
+# https://github.com/timraasveld/ansible-string-split-filter/blob/master/split.py
+
 def split_string(string, separator=None, maxsplit=-1):
     try:
         return string.split(separator, maxsplit)
     except Exception, e:
-        raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % str(e),str(string) )
+        raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % (str(e),str(string)) )
 
 def split_regex(string, separator_pattern='\s+'):
     try:
         return re.split(separator_pattern, string)
     except Exception, e:
-        raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % str(e),str(string) )
+        raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % (str(e),str(string)) )
 
 class FilterModule(object):
     ''' A filter to split a string into a list. '''
